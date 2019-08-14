d6e0cab7378d2e49be6b51dbd9decf73aea92b40
Arif BAĞLAR <arifbaglar@hotmail.com>
2019-08-14 07:59:51 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // your code here
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
